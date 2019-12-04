2449b206d6fc5a713db74ba1a2ebd3c440bdf227
Nick Lauer <Nick@nicks-air-2.hbs.edu>
2019-12-04 00:17:14 -0500

    one_time update

--- one_time.py
+++ one_time.py
@@ -6,7 +6,7 @@ db = database.cursor()
 a = input('what function do you want to do?')
 
 if a == 'create':
-    db.execute('CREATE TABLE products(store varchar(255), name varchar(255), price int, UNIQUE (name));')
+    db.execute('CREATE TABLE users(id INTEGER UNIQUE, email TEXT, hash TEXT, building TEXT, room TEXT, money DEFAULT 0, stripeID TEXT NOT NULL, PRIMARY KEY (id));')
 
 if a == 'test':
     print(len(db.execute('SELECT * FROM products').fetchall()))
