0beaaf0c4111690d5615fa089d7b55cf96b9fb48
ben-chiu <bchiu@college.harvard.edu>
2019-12-04 01:10:06 -0500

    Added catalogues for &pizza, swissbakers, crimson corner, saloniki, and anime zakka to the products list.

--- one_time.py
+++ one_time.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-database = sqlite3.connect('munus.db')
+database = sqlite3.connect('munus.db', isolation_level = None)
 db = database.cursor()
 
 a = input('what function do you want to do?')
@@ -15,4 +15,20 @@ if a == 'delete':
     db.execute('DROP TABLE users')
 
 if a == 'display':
-    for i in range(100):print(db.execute('SELECT * FROM products').fetchall()[:100][i])
+    #for i in range(100):print(db.execute('SELECT * FROM products').fetchall()[:100][i])
+    print(db.execute("SELECT * FROM products WHERE store == 'animezakka';").fetchall())
+
+if a in ['&pizza', 'saloniki','swissbakers','animezakka','crimsoncorner']:
+    f = open(a+'.txt', 'r')
+    b = f.readline()
+    b = f.readline()
+    while b:
+        print(b,'this is b')
+        b = b.replace('\n','').split(', ')
+        statement = "INSERT INTO products (store, name, price) VALUES (\"{0}\", \"{1}\", {2});".format(a, b[0], b[1])
+        print(statement)
+        try:
+            db.execute(statement)
+        except Exception as e:
+            print(e)
+        b = f.readline()
